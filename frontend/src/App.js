d74beca159c97666d46df9e0145d12e4ea360866
princehearted <princehearted795@gmail.com>
2026-04-06 18:41:29 +0300

    Fix root package.json and confirm successful React build

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Housing App Frontend</h1>
+      <p>Your React app is running!</p>
+    </div>
+  );
+}
+
+export default App;
